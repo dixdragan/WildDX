31c4990184995922aedc4fe26dab9fb185008f2e
dixdragan <dragan2dix@gmail.com>
2019-04-19 14:38:29 +0200

    Rest Delta implemented Vidik added

--- src/main/java/org/jboss/as/quickstarts/dxrest/HelloService.java
+++ src/main/java/org/jboss/as/quickstarts/dxrest/HelloService.java
@@ -33,11 +33,21 @@ import java.util.List;
  */
 public class HelloService {
 
+    private static final String line1_1 = "5:30;5:45;6:00;6:15;6:30;6:45;7:00;7:15;7:30;7:45;8:00;8:15;8:30;8:45;" +
+            "9:00;9:15;9:30;9:45;10:00;10:15;10:30;10:45;11:00;11:15;11:30;11:45;12:00;12:15;12:30;12:45;13:00;13:15;13:30;" +
+            "13:45;14:00;14:15;14:30;14:45;15:00;15:15;15:30;15:45;" +
+            "16:00;16:15;16:30;16:45;17:00;17:15;17:30;17:45;18:00;18:15;18:30;18:45;19:00;19:30;20:00;20:30;" +
+            "21:00;21:30;22:00;22:30;23:00";
     private static final String line1_2 = "5:30;5:45;6:00;6:15;6:30;6:45;7:00;7:15;7:30;7:45;8:00;8:15;8:30;8:45;" +
             "9:00;9:15;9:30;9:45;10:00;10:15;10:30;10:45;11:00;11:30;11:45;12:00;12:15;12:30;12:45;13:00;13:15;13:30;" +
             "13:45;14:00;14:15;14:30;14:45;15:00;15:15;15:30;15:45;" +
             "16:00;16:15;16:30;16:45;17:00;17:15;17:30;17:45;18:00;18:15;18:30;18:45;19:00;19:30;20:00;20:30;" +
-            "21:00;21:30;22:00;23:00";
+            "21:00;21:30;22:00;22:30;23:00";
+
+    private static final String line17_1 = "5:30;5:50;6:10;6:30;6:50;7:10;7:30;7:50;8:10;8:30;" +
+            "9:00;9:30;10:00;10:30;11:00;11:30;11:50;" +
+            "12:10;12:30;12:50;13:10;13:30;13:50;14:10;14:30;14:50;15:10;15:30;15:50;" +
+            "16:10;16:30;17:00;17:30;18:00;18:30;19:00;19:30;20:10;20:50;21:30;22:10";
 
     private static final String line17_2 = "5:30;5:50;6:10;6:30;6:50;7:10;7:30;7:50;8:10;8:30;" +
             "9:00;9:30;10:00;10:30;11:00;11:30;11:50;" +
@@ -86,14 +96,34 @@ public class HelloService {
                     }
                 }
                 break;
+            case "vidik":
+                System.out.println("VIDIK IN");
+                schedule = line1_1.split(";");
+                delayInMin = 0;
+                for (String s:schedule){
+                    LocalTime time = LocalTime.parse(s, DateTimeFormatter.ofPattern("H:mm"));
+                    System.out.println("TIME OF START: " + time);
+                    System.out.println("TIME LOOKING FROM: " + LocalTime.now(ZoneId.of("Europe/Belgrade")).minusMinutes(delayInMin + 3));
+                    if (time.isAfter(LocalTime.now(ZoneId.of("Europe/Belgrade")).minusMinutes(delayInMin + 3))){
+                        a = new Arriving("1","Nova Bolnica","Ortopedija", time, time.plusMinutes(delayInMin));
+                        ret.add(a);
+                    }
+                }
+                schedule = line17_1.split(";");
+                for (String s:schedule){
+                    LocalTime time = LocalTime.parse(s, DateTimeFormatter.ofPattern("H:mm"));
+                    if (time.isAfter(LocalTime.now(ZoneId.of("Europe/Belgrade")).minusMinutes(delayInMin + 3))){
+                        a = new Arriving("17","Nova Bolnica","Obilicevo", time, time.plusMinutes(delayInMin));
+                        ret.add(a);
+                    }
+                }
+                break;
         }
 
         Collections.sort(ret);
 
         int LIMIT = 10;
-        System.out.println("SIZE: " + ret.size());
-
-
+        //System.out.println("SIZE: " + ret.size());
         return ret.subList(0,ret.size()<LIMIT?ret.size():LIMIT);
     }
 
